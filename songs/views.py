6c596b6f378f8d7b6c4938a5ba73f7c6f2147b27
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-12-20 13:19:09 -0300

    Feat: filter songs in album by url

--- songs/views.py
+++ songs/views.py
@@ -8,6 +8,8 @@ from rest_framework.pagination import PageNumberPagination
 from .serializers import SongSerializer
 from albums.models import Album
 
+import ipdb
+
 
 class SongView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
@@ -15,6 +17,12 @@ class SongView(ListCreateAPIView, PageNumberPagination):
 
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+    def get_queryset(self):
+        # ipdb.set_trace()
+        id_album = self.kwargs['pk']
+        queryset = Song.objects.get(album=id_album)
+        return self.queryset
 
     def perform_create(self, serializer):
         return serializer.save(album_id=self.kwargs.get('pk'))
