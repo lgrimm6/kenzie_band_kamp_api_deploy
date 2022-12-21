6ab784730f97dba066099bee91b6040ea4aa8a2c
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-12-21 17:50:29 -0300

    Fix: correct in albums/<int:pk>/songs/ route

--- songs/views.py
+++ songs/views.py
@@ -1,14 +1,9 @@
 from rest_framework.generics import ListCreateAPIView
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView, Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Song
 from rest_framework.pagination import PageNumberPagination
 from .serializers import SongSerializer
-from albums.models import Album
-
-import ipdb
 
 
 class SongView(ListCreateAPIView, PageNumberPagination):
@@ -19,10 +14,8 @@ class SongView(ListCreateAPIView, PageNumberPagination):
     serializer_class = SongSerializer
 
     def get_queryset(self):
-        # ipdb.set_trace()
         id_album = self.kwargs['pk']
-        queryset = Song.objects.get(album=id_album)
-        return self.queryset
+        return self.queryset.filter(album=id_album)
 
     def perform_create(self, serializer):
         return serializer.save(album_id=self.kwargs.get('pk'))
